2ab00d2be8745152bc7da41c87b9a40f97b765b2
王鼎淇 <2070651521@qq.com>
2025-05-08 20:12:45 +0800

    5.8
    修复了无法打开文件，新建文件的问题（textArea未加载就无法获取stage）,重写getStage,这时有参

--- src/NotepadController.java
+++ src/NotepadController.java
@@ -1,5 +1,8 @@
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -65,7 +68,7 @@ public class NotepadController {
         characterCountLabel.setText(text.length() + " 个字符");
     }
 
-    @FXML private void newFile() {
+    @FXML private void newFile(ActionEvent event) {
         //如果文件被修改，提示用户保存
         if(isModified){
             Alert alert=new Alert(Alert.AlertType.CONFIRMATION);
@@ -93,7 +96,7 @@ public class NotepadController {
         textArea.clear();
         currentFile=null;
         isModified=false;
-        getStage().setTitle("无标题-记事本");
+        getStage(event).setTitle("无标题-记事本");
         lineColumnLabel.setText("行 1, 列 1");
         characterCountLabel.setText("0 个字符");
 
@@ -116,14 +119,15 @@ public class NotepadController {
 
     }
 
-    @FXML private void openFile() {//BUfferedReader和StringBuilder提高性能
+    @FXML private void openFile(ActionEvent event) {//BUfferedReader和StringBuilder提高性能
         // 创建文件选择器
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("打开文件");
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("文本文件", "*.txt")//只允许选择txt文件
         );
-        File file = fileChooser.showOpenDialog(getStage());
+//        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        File file = fileChooser.showOpenDialog(getStage(event));
         if (file != null) {
             try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
                 StringBuilder content = new StringBuilder();//创建StingBuilder对象，用于构建文件内容
@@ -141,6 +145,16 @@ public class NotepadController {
 
     private Stage getStage() {
         return (Stage) textArea.getScene().getWindow();//获取当前窗口，传入到文件选择器中
+    }
+
+    private Stage getStage(Event event) {
+        if (event.getSource() instanceof Node) {
+            Node node = (Node) event.getSource();
+            if (node.getScene() != null) {
+                return (Stage) node.getScene().getWindow();
+            }
+        }
+        return null;
     }
 
     private void showAlert(String message) {
